f6bae1c7f563f073b3c144e327ae45eae6c21877
kowsika <kowsikamurugan24@gmail.com>
2026-09-14 16:03:43 +0530

    Fix frontend API URL for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: process.env.REACT_APP_API_URL
 });
+
+console.log("API BASE URL:", process.env.REACT_APP_API_URL);
 
 // =========================================================
 // REQUEST INTERCEPTOR
